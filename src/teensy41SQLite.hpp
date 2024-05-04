76255dc497a61cf5e95f08f34bbef99d7ac1195a
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-05-04 15:10:25 +0200

    initial WIP version

---
+++ src/teensy41SQLite.hpp
@@ -0,0 +1,18 @@
+#include "sqlite3.h"
+
+#include <SdFat.h>
+
+sqlite3_vfs *sqlite3_demovfs();
+SdFat* vfsFilesystem;
+
+int setupSQLite(SdFat* io_filesystem)
+{
+  if (not io_filesystem)
+  {
+    return SQLITE_ERROR;
+  }
+
+  vfsFilesystem = io_filesystem;
+
+  return sqlite3_vfs_register(sqlite3_demovfs(), 1);
+}
